947f840fff92d66f89605595e863a5fd1b2e39a8
raistlinJ <raistlinJ@users.noreply.github.com>
2019-08-07 16:51:35 -0600

    Fixed incorrect references

--- src/main/python/WiresharkRunner.py
+++ src/main/python/WiresharkRunner.py
@@ -8,22 +8,22 @@ class WiresharkRunner(QThread):
     WIRESHARK_PATH="wireshark"
 
     def __init__(self, lua_scripts=None, pcap_filename=None):
-        logging.info('WiresharkWindow(): Instantiated')
+        logging.info('WiresharkRunner(): Instantiated')
         QThread.__init__(self)
-        self.cmd = WiresharkWindow.WIRESHARK_PATH
+        self.cmd = WiresharkRunner.WIRESHARK_PATH
         if pcap_filename != None:
             self.cmd+= " -r " + pcap_filename
         if lua_scripts != None and len(lua_scripts) > 0:
             for lua_script in lua_scripts:
                 self.cmd+= " -Xlua_script:" + lua_script
-        logging.info('WiresharkWindow(): Complete')
+        logging.info('WiresharkRunner(): Complete')
 
     def run(self):
-        logging.info('WiresharkWindow.run(): Instantiated')
+        logging.info('WiresharkRunner.run(): Instantiated')
         if sys.platform == "linux" or sys.platform == "linux2":
             output = subprocess.check_output(shlex.split(self.cmd), encoding="utf-8")
         else: 
             output = subprocess.check_output(self.cmd, encoding="utf-8")
 
-        logging.info('WiresharkWindow.run(): Complete')
+        logging.info('WiresharkRunner.run(): Complete')
 
