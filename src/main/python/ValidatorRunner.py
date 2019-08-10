aa7a83dbe1797b2b2e254ee55d8de9aad9460821
raistlinJ <raistlinJ@users.noreply.github.com>
2019-08-09 22:20:31 -0600

    The scoring results are now displayed when the validate button is pressed.

--- src/main/python/ValidatorRunner.py
+++ src/main/python/ValidatorRunner.py
@@ -8,12 +8,15 @@ from PyQt5.QtCore import QThread
 class ValidatorRunner(QThread):
     VALIDATOR_PATH = "/root/git/eceld-traffic-validator/scripts/"
     VALIDATOR_FILENAME = "validate_train.sh"
+    VALIDATOR_SOLN_PATH = "/root/git/eceld-traffic-validator/sample-ranking-files/"
+    VALIDATOR_SOLN_FILENAME = "train_soln.JSON"
 
     def __init__(self, commented_pcap_filename, validate_pcap_filename):
         logging.info('ValidatorRunner(): Instantiated')
         QThread.__init__(self)
         self.cmd = os.path.join(ValidatorRunner.VALIDATOR_PATH, ValidatorRunner.VALIDATOR_FILENAME)
         self.cmd = self.cmd + " " + commented_pcap_filename + " " + validate_pcap_filename
+        self.cmd = self.cmd + " " + os.path.join(ValidatorRunner.VALIDATOR_SOLN_PATH, ValidatorRunner.VALIDATOR_SOLN_FILENAME)
         logging.info('ValidatorRunner(): Complete')
 
     def run(self):
